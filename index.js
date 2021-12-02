f620f70f72d9a11ec8e6e95b777d815d9dd14845
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2021-12-02 21:37:01 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var myPhoneNumber=95435435
+var myName ="the man";
+
+console.log(isNaN(myPhoneNumber));
+console.log(isNaN(myName));
+ // if can we this case for check the data is number or not 
+ // in this code :if we put  myName-- the out will be the plese enter the valid number and   vice versa 
+if(isNaN(myPhoneNumber)){
+    console.log("please enter valid phone number");
+    
+}
+
